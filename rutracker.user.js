006f93a27f811f0b2dcb01fba2b6340a85e29bfa
Bui The Hoa <buithehoa@gmail.com>
2019-09-08 01:55:30 -0400

    Copy magnetic link.

--- rutracker.user.js
+++ rutracker.user.js
@@ -20,16 +20,15 @@ $(document).ready(function() {
 
   // Copy magnet link to clipboard
   if ($('#topic_main').length) {
-    setClipboardText($('a.magnet-link').attr('href'));
+    setClipboardText($('a.med.magnet-link-1').attr('href'));
     $('#topic_main').get(0).scrollIntoView();
 
     setTimeout(function() {
       var magnetIcon = "<img\
-				class='magnet-icon'\
-				style='display: none'\
-				src='//static.t-ru.org/templates/v1/images/icon_magnet_16_1.png'\
-				alt='magnet' />";
-      $('#topic_main .post_body > span:first-child').append(magnetIcon);
+        src='//static.t-ru.org/templates/v1/images/magnet_1.svg'\
+        style='width: 1em; margin-left: 0.5em;'\
+        alt='magnet' />";
+      $('#topic_main tbody:nth-child(2) .post_body > span:first-child').append(magnetIcon);
 
       $('.magnet-icon').css('margin-left', '0.5em');
       $('.magnet-icon').fadeIn({
@@ -85,9 +84,10 @@ function setClipboardText(text) {
     if (!status) {
       console.error("Cannot copy text");
     } else {
-      console.log("The text is now on the clipboard");
+      console.log("The text is now on the clipboard", text);
     }
   } catch (err) {
     console.log('Unable to copy.');
   }
 }
+
