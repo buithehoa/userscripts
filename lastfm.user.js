865a99c009121ec0fe986a0c1b4eed1becd75cda
Bui The Hoa <buithehoa@gmail.com>
2020-10-18 22:13:17 +0700

    Add lastfm.user.js

---
+++ lastfm.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name last.fm
+// @version 1.0
+// @description Sort albums in descending order by play count.
+//
+// @match *://rutracker.org/forum/*
+// @match *://www.last.fm/user/buithehoa/library/albums?date_preset=LAST_30_DAYS*
+//
+// @require https://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+var waitForEl = function(selector, callback, count) {
+  var MAX_NUMBER_OF_RETRIES = 60;
+  var TIMEOUT = 1000;
+
+  if (jQuery(selector).length) {
+    callback();
+  } else {
+    setTimeout(function() {
+      if (!count) {
+        count = 0;
+      }
+      count++;
+      if (count <= MAX_NUMBER_OF_RETRIES) {
+        waitForEl(selector, callback, count);
+      } else {return;}
+    }, TIMEOUT);
+  }
+};
+
+$(document).ready(function() {
+  waitForEl('.chartlist .chartlist-row', function() {
+    $('.chartlist .chartlist-row').sort(function(a, b) {
+      return 1;
+    }).appendTo('.chartlist');
+  });
+});
+
