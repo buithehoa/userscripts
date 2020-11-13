48f3e26590081a9a9fcbd315eaeb7fc50a31bd51
Bui The Hoa <buithehoa@gmail.com>
2020-11-13 18:05:25 +0700

    Remove scroll into view.

--- lastfm.user.js
+++ lastfm.user.js
@@ -34,9 +34,5 @@ $(document).ready(function() {
     $('.chartlist .chartlist-row').sort(function(a, b) {
       return 1;
     }).appendTo('.chartlist');
-
-    if ($('#top-albums-section').length) {
-      $('#top-albums-section').get(0).scrollIntoView();
-    }
   });
 });
